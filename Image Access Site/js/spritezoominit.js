68fa5d560018d04e76b657a38541ec68a7639497
TedKharkivTeam <tedkharkivteam@gmail.com>
2016-04-06 10:43:05 +0300

    Maxim comeback
    fixed wideteck 48c

---
+++ Image Access Site/js/spritezoominit.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".zoom1").spritezoom({
+        border  : 6,
+        source  : "../images/magnifier/WT48C-600_RENDER_DIAGONAL_big.png",
+        zSource : "../images/magnifier/WT48C-600_RENDER_DIAGONAL_big1.png",
+        layout  : "right",
+        title   : "Scanner"
+    });
+});
