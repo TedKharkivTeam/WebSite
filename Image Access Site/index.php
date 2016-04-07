ccd762f4af65bc90216eaa838d8cd4118a6f4ac6
TedKharkivTeam <tedkharkivteam@gmail.com>
2016-04-07 21:26:18 +0300

    PHP Test DO NOT TOUCH IT

--- Image Access Site/index.php
+++ Image Access Site/index.php
@@ -1,9 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Home page</title>
-<meta http-equiv="refresh" content="0; url=HTML_Import.html"/>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="cache-control" content="no-cache">
+	
+	<title>Home page</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/homepagepictures.css">
+    <link rel="stylesheet" type="text/css" href="css/fixedlogo.css">
+    <link rel="stylesheet" type="text/css" href="css/fixedbottomlogo.css">
+    <link rel="stylesheet" type="text/css" href="css/content.css">
+    <link rel="stylesheet" type="text/css" href="css/gradient_background.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/mainmenu.css">
+    <link rel="stylesheet" type="text/css" href="css/topbar.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	
+	<!-- HTML IMPORT LINK -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	
+	<!--<script type="text/javascript" src="js/autozoom.js"></script>-->
+	<!--<script type="text/javascript" src="js/geolocation.js"></script>-->
+	
 </head>
-<body>
+<body class="gradient">
+
+	<!-- HEADER PASTE -->
+	<?php include("includes/header.html");?>
+			
+	<div class = "content">
+		<div id="container">
+			
+		</div>
+	
+		<div class = "logo">
+			<img src = "images/IALogo.jpg">
+		</div>
+	
+		<img id="dlsgImage" class = "homepagepictures" src="images/dlsg.jpg">
+		<img id="bscanImage" class = "homepagepictures" src="images/bscan.jpg">
+	</div>
+	
+	<!-- FOOTER PASTE -->
+	<?php include("includes/footer.html");?>
 </body>
 </html>
