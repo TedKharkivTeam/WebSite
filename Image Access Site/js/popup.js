c02ef5651f5e0664c4dc78b78d6011cb53675ad8
AlphaFly <serj.donskoj.2012@gmail.com>
2016-04-22 22:09:47 +0300

    Ez buttons for ez forms

---
+++ Image Access Site/js/popup.js
@@ -0,0 +1,10 @@
+ function openbox(id,tt) {
+	var div = document.getElementById(id);
+	var tt_div = document.getElementById(tt);
+	if(div.style.display == 'block') {
+		div.style.display = 'none';
+	}
+	else {
+		div.style.display = 'block';
+	}
+  }
