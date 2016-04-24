2cd13ff4f30420fead66e8c972da1053c6d58ce3
EduardChernikh <Eduard.chernikh@gmail.com>
2016-04-24 14:49:49 +0300

    Map zooming adjustments

--- Image Access Site/js/service.js
+++ Image Access Site/js/service.js
@@ -26,6 +26,8 @@ $(function () {
         ext: 'png'
     }).addTo(map);
     map.on('zoomend', function (e) {
+        console.log(e.target._zoom);
+
         if (e.target._zoom <= 2) {
             map.scrollWheelZoom.disable();
         } else {
@@ -365,7 +367,7 @@ $(function () {
 
         switch (region) {
             case "US":
-                map.fitBounds(UnitedStatesAndCanada_LayerGroup.getBounds());
+                map.setView({lat: 54.77534585936447, lng: -95.67306518554689}, 4);
                 $('.leaflet-control-zoom').show();
                 break;
             case "CAR":
@@ -377,11 +379,11 @@ $(function () {
                 $('.leaflet-control-zoom').show();
                 break;
             case "SOA":
-                map.fitBounds(SouthAmerica_LayerGroup.getBounds());
+                map.setView({lat: -15.961329081596647, lng: -62.67425537109376}, 4);
                 $('.leaflet-control-zoom').show();
                 break;
             case "EU":
-                map.fitBounds(Europe_LayerGroup.getBounds());
+                map.setView({lat: 58.28940590271679, lng: 13.774795532226564}, 4);
                 $('.leaflet-control-zoom').show();
                 break;
             case "ME":
@@ -389,7 +391,7 @@ $(function () {
                 $('.leaflet-control-zoom').show();
                 break;
             case "AS":
-                map.fitBounds(Asia_LayerGroup.getBounds());
+                map.setView({lat: 31.728167146023935, lng: 98.16873168945314}, 4);
                 $('.leaflet-control-zoom').show();
                 break;
             case "AU":
@@ -397,7 +399,7 @@ $(function () {
                 $('.leaflet-control-zoom').show();
                 break;
             case "AF":
-                map.fitBounds(Africa_LayerGroup.getBounds());
+                map.setView({lat: 3.36246686553575, lng: 19.99099731445313}, 4);
                 $('.leaflet-control-zoom').show();
                 break;
             case "WW":
@@ -406,7 +408,10 @@ $(function () {
                 $('.leaflet-control-zoom').hide();
         }
     });
+
+    map.on('click', function(e) {console.log(e.latlng)});
 });
+
 
 
 
