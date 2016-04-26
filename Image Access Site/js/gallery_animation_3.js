73fdaedebfe58847769e72c483bf097d3a04f429
TedKharkivTeam <tedkharkivteam@gmail.com>
2016-04-26 11:18:09 +0300

    Removed inline scripts on frames pages

--- Image Access Site/js/gallery_animation_3.js
+++ Image Access Site/js/gallery_animation_3.js
@@ -48,146 +48,153 @@ var animationPresets = {
 };
 
 function aminate(onAnimationComplete) {
-    $(document).ready(function () {
-        var canvas = document.getElementById("animation-canvas");
-        var context = canvas.getContext("2d");
-        context.translate(0.5, 0.5);
+    var canvas = document.getElementById("animation-canvas");
+    var context = canvas.getContext("2d");
+    context.translate(0.5, 0.5);
 
-        var allImages = $(".gallery-container .gallery-image");
-        var $container = $(".gallery-container");
-        var images = $(".gallery-container .animate-image");
-        var animatedImages = [];
-        imagesCount = images.length;
+    var $container = $(".gallery-container");
+    var images = $(".gallery-container .animate-image");
+    var animatedImages = [];
+    imagesCount = images.length;
 
-        // setting canvas height and width equals to container;
-        var $canvas = $("#animation-canvas");
+    // setting canvas height and width equals to container;
+    var $canvas = $("#animation-canvas");
+    setTimeout(function () {
+        $canvas.attr("width", $container.width());
+        $canvas.attr("height", $container.height());
+    }, 500);
+
+    function getPreset(count) {
+        if (count === 3) {
+            return animationPresets["preset_for_3_images"];
+        } else {
+            return animationPresets["preset_for_4_images"];
+        }
+    }
+
+    (function animateImages(number, timeout) {
         setTimeout(function () {
-            $canvas.attr("width", $container.width());
-            $canvas.attr("height", $container.height());
-        }, 500);
+            var preset = getPreset(number);
+            images.each(function (index) {
+                var $defaultImage = $(this);
+                var $image = $(this).clone().addClass("animated-image-block").appendTo($container);
+                var $image2 = $(this).clone().appendTo($container);
 
-        function getPreset(count) {
-            if (count === 3) {
-                return animationPresets["preset_for_3_images"];
-            } else {
-                return animationPresets["preset_for_4_images"];
-            }
-        }
+                var $imgPosition = $defaultImage.position();
+                var defaultWidth = $defaultImage.width();
+                var defaultHeight = $defaultImage.height();
 
-        (function animateImages(number, timeout) {
-            setTimeout(function () {
-                var preset = getPreset(number);
-                images.each(function (index) {
-                    var $defaultImage = $(this);
-                    var $image = $(this).clone().addClass("animated-image-block").appendTo($container);
-                    var $image2 = $(this).clone().appendTo($container);
-
-                    var $imgPosition = $defaultImage.position();
-                    var defaultWidth = $defaultImage.width();
-                    var defaultHeight = $defaultImage.height();
-
-                    $image.css({left: $imgPosition.left, top: $imgPosition.top, "z-index": 20000 + index});
-                    $image2.css({
-                        left: $imgPosition.left,
-                        top: $imgPosition.top,
-                        "z-index": 10000 + index,
-                        position: "absolute"
-                    });
-                    animatedImages.push({
-                        default_position: $imgPosition,
-                        default_width: defaultWidth,
-                        default_height: defaultHeight,
-                        clone_img: $image
-                    });
-
-                    $image.animate(preset["image_" + index].stage1, animationDuration, function onFirstStageComplete() {
-                        $image.animate(preset["image_" + index].stage2, animationDuration / 3, onAnimationComplete);
-                    });
+                $image.css({left: $imgPosition.left, top: $imgPosition.top, "z-index": 20000 + index});
+                $image2.css({
+                    left: $imgPosition.left,
+                    top: $imgPosition.top,
+                    "z-index": 10000 + index,
+                    position: "absolute"
                 });
-                $(".gallery-container .gallery-image:not(.animated-image-block)").animate({
-                    opacity: 0.7
-                }, animationDuration);
-                $canvas.fadeOut(animationDuration + 1000);
-                render();
-            }, timeout);
-        })(imagesCount, startTimeout);
+                animatedImages.push({
+                    default_position: $imgPosition,
+                    default_width: defaultWidth,
+                    default_height: defaultHeight,
+                    clone_img: $image
+                });
 
-        function render() {
-            // clear canvas
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.fillStyle = "#35ea1c";
-            if (animatedImages.length > 0) {
-                animatedImages.forEach(function (img) {
-                    var pointsObj = getImagePositions(img);
-                    var startPoints = pointsObj.startPoints;
-                    var endPoints = pointsObj.endPoints;
+                $image.animate(preset["image_" + index].stage1, animationDuration, function onFirstStageComplete() {
+                    $image.animate(preset["image_" + index].stage2, animationDuration / 3, onAnimationComplete);
+                });
+            });
+            $(".gallery-container .gallery-image:not(.animated-image-block)").animate({
+                opacity: 0.7
+            }, animationDuration);
+            $canvas.fadeOut(animationDuration + 1000);
+            render();
+        }, timeout);
+    })(imagesCount, startTimeout);
 
-                    if (startPoints.center.top <= endPoints.center.top) {
-                        if (startPoints.center.left >= endPoints.center.left) {
-                            context.beginPath();
-                            context.moveTo(endPoints.top_left.left, endPoints.top_left.top);
-                            context.lineTo(startPoints.top_left.left, startPoints.top_left.top);
-                            context.lineTo(startPoints.bottom_right.left, startPoints.bottom_right.top);
-                            context.lineTo(endPoints.bottom_right.left, endPoints.bottom_right.top);
-                            context.fill();
-                        } else {
-                            context.beginPath();
-                            context.moveTo(endPoints.top_right.left, endPoints.top_right.top);
-                            context.lineTo(startPoints.top_right.left, startPoints.top_right.top);
-                            context.lineTo(startPoints.bottom_left.left, startPoints.bottom_left.top);
-                            context.lineTo(endPoints.bottom_left.left, endPoints.bottom_left.top);
-                            context.fill();
-                        }
+    function render() {
+        // clear canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "#35ea1c";
+        if (animatedImages.length > 0) {
+            animatedImages.forEach(function (img) {
+                var pointsObj = getImagePositions(img);
+                var startPoints = pointsObj.startPoints;
+                var endPoints = pointsObj.endPoints;
+
+                if (startPoints.center.top <= endPoints.center.top) {
+                    if (startPoints.center.left >= endPoints.center.left) {
+                        context.beginPath();
+                        context.moveTo(endPoints.top_left.left, endPoints.top_left.top);
+                        context.lineTo(startPoints.top_left.left, startPoints.top_left.top);
+                        context.lineTo(startPoints.bottom_right.left, startPoints.bottom_right.top);
+                        context.lineTo(endPoints.bottom_right.left, endPoints.bottom_right.top);
+                        context.fill();
                     } else {
-                        if (startPoints.center.left >= endPoints.center.left) {
-                            // draw right to left rect
-                            context.beginPath();
-                            context.moveTo(endPoints.top_right.left, endPoints.top_right.top);
-                            context.lineTo(startPoints.top_right.left, startPoints.top_right.top);
-                            context.lineTo(startPoints.bottom_left.left, startPoints.bottom_left.top);
-                            context.lineTo(endPoints.bottom_left.left, endPoints.bottom_left.top);
-                            context.fill();
-                        } else {
-                            // draw left to right rect
-                            context.beginPath();
-                            context.moveTo(endPoints.top_left.left, endPoints.top_left.top);
-                            context.lineTo(startPoints.top_left.left, startPoints.top_left.top);
-                            context.lineTo(startPoints.bottom_right.left, startPoints.bottom_right.top);
-                            context.fill();
-                        }
+                        context.beginPath();
+                        context.moveTo(endPoints.top_right.left, endPoints.top_right.top);
+                        context.lineTo(startPoints.top_right.left, startPoints.top_right.top);
+                        context.lineTo(startPoints.bottom_left.left, startPoints.bottom_left.top);
+                        context.lineTo(endPoints.bottom_left.left, endPoints.bottom_left.top);
+                        context.fill();
                     }
-                });
-            }
-            requestAnimationFrame(render);
-        }
-
-        function getImagePositions(obj) {
-            // start data
-            var startPos = obj.default_position;
-            var startWidth = obj.default_width;
-            var startHeight = obj.default_height;
-
-            // current image data
-            var imgPos = obj.clone_img.position();
-            var imgWidth = obj.clone_img.width();
-            var imgHeight = obj.clone_img.height();
-
-            return {
-                startPoints: { // bounding box for starting image
-                    top_left: {top: startPos.top, left: startPos.left},
-                    top_right: {top: startPos.top, left: startPos.left + startWidth},
-                    bottom_left: {top: startPos.top + startHeight, left: startPos.left},
-                    bottom_right: {top: startPos.top + startHeight, left: startPos.left + startWidth},
-                    center: {top: startPos.top + startHeight / 2, left: startPos.left + startWidth / 2}
-                },
-                endPoints: { // bounding box for animated image
-                    top_left: {top: imgPos.top, left: imgPos.left},
-                    top_right: {top: imgPos.top, left: imgPos.left + imgWidth},
-                    bottom_left: {top: imgPos.top + imgHeight, left: imgPos.left},
-                    bottom_right: {top: imgPos.top + imgHeight, left: imgPos.left + imgWidth},
-                    center: {top: imgPos.top + imgHeight / 2, left: imgPos.left + imgWidth / 2}
+                } else {
+                    if (startPoints.center.left >= endPoints.center.left) {
+                        // draw right to left rect
+                        context.beginPath();
+                        context.moveTo(endPoints.top_right.left, endPoints.top_right.top);
+                        context.lineTo(startPoints.top_right.left, startPoints.top_right.top);
+                        context.lineTo(startPoints.bottom_left.left, startPoints.bottom_left.top);
+                        context.lineTo(endPoints.bottom_left.left, endPoints.bottom_left.top);
+                        context.fill();
+                    } else {
+                        // draw left to right rect
+                        context.beginPath();
+                        context.moveTo(endPoints.top_left.left, endPoints.top_left.top);
+                        context.lineTo(startPoints.top_left.left, startPoints.top_left.top);
+                        context.lineTo(startPoints.bottom_right.left, startPoints.bottom_right.top);
+                        context.fill();
+                    }
                 }
+            });
+        }
+        requestAnimationFrame(render);
+    }
+
+    function getImagePositions(obj) {
+        // start data
+        var startPos = obj.default_position;
+        var startWidth = obj.default_width;
+        var startHeight = obj.default_height;
+
+        // current image data
+        var imgPos = obj.clone_img.position();
+        var imgWidth = obj.clone_img.width();
+        var imgHeight = obj.clone_img.height();
+
+        return {
+            startPoints: { // bounding box for starting image
+                top_left: {top: startPos.top, left: startPos.left},
+                top_right: {top: startPos.top, left: startPos.left + startWidth},
+                bottom_left: {top: startPos.top + startHeight, left: startPos.left},
+                bottom_right: {top: startPos.top + startHeight, left: startPos.left + startWidth},
+                center: {top: startPos.top + startHeight / 2, left: startPos.left + startWidth / 2}
+            },
+            endPoints: { // bounding box for animated image
+                top_left: {top: imgPos.top, left: imgPos.left},
+                top_right: {top: imgPos.top, left: imgPos.left + imgWidth},
+                bottom_left: {top: imgPos.top + imgHeight, left: imgPos.left},
+                bottom_right: {top: imgPos.top + imgHeight, left: imgPos.left + imgWidth},
+                center: {top: imgPos.top + imgHeight / 2, left: imgPos.left + imgWidth / 2}
             }
         }
-    });
+    }
 }
+
+$(document).ready(function () {
+    fadeIn(function () {
+        aminate(function () {
+            simulateAnimation(function () {
+                redirect("Frame_4A.html");
+            });
+        });
+    });
+});
